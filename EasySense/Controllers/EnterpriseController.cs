890d5231c63668c44b7b45828aed7d80060881b9
あまみや ゆうこ <1@4321.io>
2015-02-02 13:21:29 +0800

    111

--- EasySense/Controllers/EnterpriseController.cs
+++ EasySense/Controllers/EnterpriseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EasySense.Models;
 
 namespace EasySense.Controllers
 {
@@ -13,6 +14,14 @@ namespace EasySense.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Search(string Text)
+        {
+            var result = new List<EnterpriseListViewModel>();
+            //TODO: 完成根据Key字段Contains搜索结果与Title字段Contains搜索结果
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- EasySense/Models/SuperSearchViewModel.cs
+++ EasySense/Models/SuperSearchViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace EasySense.Controllers
+namespace EasySense.Models
 {
     public class SuperSearchViewModel
     {
