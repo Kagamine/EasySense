351e7ad6dc2b2772d2d523f40684b744752806d6
あまみや ゆうこ <1@4321.io>
2015-02-02 12:43:57 +0800

    111

---
+++ EasySense/Models/EnterpriseListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySense.Models
+{
+    public class EnterpriseListViewModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+        public static implicit operator EnterpriseListViewModel(EnterpriseModel Enterprise)
+        {
+            return new EnterpriseListViewModel
+            {
+                ID = Enterprise.ID,
+                Level = Enterprise.Level.ToString(),
+                Title = Enterprise.Key[0] + "-" + Enterprise.Title
+            };
+        }
+    }
+}
