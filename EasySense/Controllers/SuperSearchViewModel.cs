496ea44bf9cca4046feec7cb72b4ae2cee1b1da0
あまみや ゆうこ <1@4321.io>
2015-02-02 13:13:30 +0800

    111

---
+++ EasySense/Controllers/SuperSearchViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySense.Controllers
+{
+    public class SuperSearchViewModel
+    {
+        public List<SuperSearchProjectViewModel> Projects { get; set; }
+        public List<SuperSearchEnterpriseViewModel> Enterprises { get; set; }
+        public List<SuperSearchCustomerViewModel> Customers { get; set; }
+        public List<SuperSearchUserViewModel> Users { get; set; }
+        public List<SuperSearchFileViewModel> Files { get; set; }
+    }
+    public class SuperSearchProjectViewModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+    public class SuperSearchEnterpriseViewModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Level { get; set; }
+    }
+    public class SuperSearchUserViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class SuperSearchCustomerViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Enterprise { get; set; }
+    }
+    public class SuperSearchFileViewModel
+    {
+        public string ID { get; set; }
+        public string Filename { get; set; }//without extension, e.g. "HelloWorld"
+        public string Extension { get; set; }//with dot, e.g. ".exe"
+        public DateTime Time { get; set; }
+    }
+}
