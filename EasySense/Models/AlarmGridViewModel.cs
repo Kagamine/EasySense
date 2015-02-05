316272973d5a5c817962692f6a36064eb3749525
あまみや ゆうこ <1@4321.io>
2015-02-05 15:07:20 +0800

    123

---
+++ EasySense/Models/AlarmGridViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySense.Models
+{
+    public class AlarmGridViewModel
+    {
+        public string title { get; set; }
+
+        public DateTime begin { get; set; }
+
+        public DateTime end { get; set; }
+
+        public bool allDay { get; set; }
+
+        public string url { get; set; }
+
+        public static implicit operator AlarmGridViewModel(AlarmModel Alarm)
+        {
+
+        }
+    }
+}
