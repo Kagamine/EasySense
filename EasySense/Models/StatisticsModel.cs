bf890e8d0491ac5091379a2ea406b736035c0db6
あまみや ゆうこ <1@4321.io>
2015-02-07 21:44:29 +0800

    111

---
+++ EasySense/Models/StatisticsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasySense.Models
+{
+    public class StatisticsModel
+    {
+        public Guid ID { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string Title { get; set; }
+
+        public UserRole? PushTo { get; set; }
+
+        public string Hint { get; set; }
+
+        public 
+    }
+}
