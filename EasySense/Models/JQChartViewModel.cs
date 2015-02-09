fb8ba0e31896b265cb5eab0a9927a9fdabf4523e
あまみや ゆうこ <1@4321.io>
2015-02-09 15:28:11 +0800

    123

---
+++ EasySense/Models/JQChartViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySense.Models
+{
+    public class JQChartViewModel
+    {
+        public string type { get; set; }
+
+        public string title { get; set; }
+
+        public List<dynamic[]> data { get; set; }
+
+        public void PushData(string Key, int Value)
+        {
+            dynamic[] a = new dynamic[2];
+            a[0] = Key;
+            a[1] = Value;
+            data.Add(a);
+        }
+    }
+}
