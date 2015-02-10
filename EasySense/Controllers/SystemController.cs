030d2dcac434738516bdaef06166fef3497b2138
あまみや ゆうこ <1@4321.io>
2015-02-10 15:59:29 +0800

    123

--- EasySense/Controllers/SystemController.cs
+++ EasySense/Controllers/SystemController.cs
@@ -136,5 +136,56 @@ namespace EasySense.Controllers
             DB.SaveChanges();
             return RedirectToAction("Category", "System");
         }
+
+        [MinRole(UserRole.Root)]
+        public ActionResult Department()
+        {
+            var departments = (from d in DB.Departments
+                               orderby d.ID descending
+                               select d).ToList();
+            return View(departments);
+        }
+
+        [HttpPost]
+        [ValidateSID]
+        [MinRole(UserRole.Root)]
+        public ActionResult CreateDepartment(DepartmentModel Model)
+        {
+            if (Model.UserID != null)
+            {
+                var user = DB.Users.Find(Model.UserID);
+                if (user.Role < UserRole.Master)
+                    user.Role = UserRole.Master;
+            }
+            DB.Departments.Add(Model);
+            DB.SaveChanges();
+            return RedirectToAction("Department", "System");
+        }
+
+        [HttpPost]
+        [ValidateSID]
+        [MinRole(UserRole.Root)]
+        public ActionResult EditDepartment(int id, string Title, int? UserID)
+        {
+            var department = DB.Departments.Find(id);
+            department.Title = Title;
+            department.UserID = UserID;
+            if (UserID.HasValue)
+            {
+                var user = DB.Users.Find(UserID.Value);
+                if (user.Role < UserRole.Master)
+                    user.Role = UserRole.Master;
+            }
+            DB.SaveChanges();
+            return RedirectToAction("Department", "System");
+        }
+
+        [HttpGet]
+        [MinRole(UserRole.Root)]
+        public ActionResult GetDepartment(int id)
+        {
+            var department = DB.Departments.Find(id);
+            return Json((DepartmentModel)department, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ EasySense/Models/DepartmentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasySense.Models
+{
+    public class DepartmentViewModel
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public int? UserID { get; set; }
+
+        public static implicit operator DepartmentViewModel(DepartmentModel Model)
+        {
+            return new DepartmentViewModel
+            {
+                ID = Model.ID,
+                Title = Model.Title,
+                UserID = Model.UserID
+            };
+        }
+    }
+}
